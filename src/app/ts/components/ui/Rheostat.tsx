b8d0179cbc9ac74312f79621e23afb6adb0afb73
ruslan <ruslanchek@gmail.com>
2018-06-25 11:28:53 +0300

    Modal

--- src/app/ts/components/ui/Rheostat.tsx
+++ src/app/ts/components/ui/Rheostat.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import { css, StyleDeclaration, StyleSheet } from 'aphrodite/no-important';
-import { ObjectsStore } from '../../stores/ObjectsStore';
-import IObject = ObjectsStore.IObject;
 import { COLORS, THEME } from '../../theme';
 import { NumberFormat } from './NumberFormat';
+import { CSSUtils } from '../../lib/CSSUtils';
 
 interface IProps {
 	min: number;
@@ -15,14 +14,14 @@ interface IProps {
 interface IState {
 	currentMin: number;
 	currentMax: number;
-	width: number;
+	boundId: number;
 }
 
 export class Rheostat extends React.PureComponent<IProps, IState> {
 	public state: IState = {
 		currentMin: 0,
 		currentMax: 100,
-		width: 0
+		boundId: null
 	};
 
 	private bar = null;
@@ -32,24 +31,124 @@ export class Rheostat extends React.PureComponent<IProps, IState> {
 			currentMin: this.props.currentMin,
 			currentMax: this.props.currentMax
 		});
-
-		this.countSize();
 	}
 
-
 	public render() {
-		const {currentMin, currentMax, width} = this.state;
+		const {currentMin, currentMax} = this.state;
 
 		return (
-			<div className={css(styles.container)}>
+			<div
+				className={css(styles.container)}
+				onTouchMove={(e) => {
+					if(e.touches && e.touches[0]) {
+						this.calculatePositions(e.touches[0].clientX);
+					}
+				}}
+				onMouseMove={(e: any) => {
+					this.calculatePositions(e.nativeEvent.clientX);
+				}}
+				onClick={(e: any) => {
+					this.calculatePositions(e.nativeEvent.clientX);
+				}}
+				onMouseLeave={() => {
+					this.setState({
+						boundId: null
+					});
+				}}
+				onMouseUp={() => {
+					this.setState({
+						boundId: null
+					});
+				}}
+				onTouchEnd={() => {
+					this.setState({
+						boundId: null
+					});
+				}}
+				onTouchCancel={() => {
+					this.setState({
+						boundId: null
+					});
+				}}
+			>
 				<div className={css(styles.title)}>
 					Price range
 				</div>
 
-				<div className={css(styles.bar)} ref={(ref) => this.bar = ref}>
-					<i className={css(styles.line)} style={{left: this.getValuePosition(currentMin) + '%', right: 100 - this.getValuePosition(currentMax) + '%'}}/>
-					<i className={css(styles.handle)} style={{left: this.getValuePosition(currentMin) + '%'}}/>
-					<i className={css(styles.handle)} style={{left: this.getValuePosition(currentMax) + '%'}}/>
+				<div
+					className={css(styles.bar)}
+					ref={(ref) => this.bar = ref}
+				>
+					<i
+						className={css(styles.line)}
+						style={{left: this.getValuePosition(currentMin) + '%', right: 100 - this.getValuePosition(currentMax) + '%'}}
+					/>
+
+					<i
+						onTouchStart={() => {
+							this.setState({
+								boundId: 1
+							});
+						}}
+						onMouseDown={() => {
+							this.setState({
+								boundId: 1
+							});
+						}}
+						onTouchEnd={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						onTouchCancel={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						onMouseUp={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						{...CSSUtils.mergeStyles(
+							css(styles.handle),
+							this.state.boundId === 1 && css(styles.handleActive)
+						)}
+						style={{left: this.getValuePosition(currentMin) + '%'}}
+					/>
+
+					<i
+						onTouchStart={() => {
+							this.setState({
+								boundId: 2
+							});
+						}}
+						onMouseDown={() => {
+							this.setState({
+								boundId: 2
+							});
+						}}
+						onTouchEnd={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						onTouchCancel={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						onMouseUp={() => {
+							this.setState({
+								boundId: null
+							});
+						}}
+						{...CSSUtils.mergeStyles(
+							css(styles.handle),
+							this.state.boundId === 2 && css(styles.handleActive)
+						)}
+						style={{left: this.getValuePosition(currentMax) + '%'}}
+					/>
 
 					<span className={css(styles.value, styles.minValue)}>
 						<NumberFormat value={currentMin}/>
@@ -63,23 +162,43 @@ export class Rheostat extends React.PureComponent<IProps, IState> {
 		);
 	}
 
-	private getValuePosition(value: number) {
+	private calculatePositions(clientX: number): void {
+		const box: ClientRect = this.bar.getBoundingClientRect();
+		const position: number = clientX - box.left;
+		const value: number = this.getPositionValue(position);
+
+		if(value < this.props.min || value > this.props.max) {
+			return;
+		}
+
+		if(this.state.boundId === 2) {
+			this.setState({
+				currentMax: value
+			});
+		}
+
+		if(this.state.boundId === 1) {
+			this.setState({
+				currentMin: value
+			});
+		}
+	}
+
+	private getValuePosition(value: number): number {
 		const minMax: number = this.props.max - this.props.min;
 		return (minMax / 100) * value;
 	}
 
-	private countSize(): void {
+	private getPositionValue(position: number): number {
 		const barBox: ClientRect = this.bar.getBoundingClientRect();
-
-		this.setState({
-			width: barBox.width
-		});
+		return position / (barBox.width / 100);
 	}
 }
 
 const styles = StyleSheet.create({
 	container: {
-		marginBottom: 40
+		paddingBottom: 40,
+		userSelect: 'none'
 	},
 
 	value: {
@@ -127,12 +246,22 @@ const styles = StyleSheet.create({
 		backgroundColor: COLORS.GRAY_LIGHT.toString(),
 		boxShadow: THEME.BOX_SHADOW_ELEVATION_1,
 		cursor: 'pointer',
-		transition: 'all .2s',
+		transition: 'transform .2s, background-color .2s',
 		border: `2px solid ${COLORS.WHITE.toString()}`,
 
 		':hover': {
 			backgroundColor: COLORS.BLUE.lighten(1.2).toString(),
 			transform: 'translate(-50%, -50%) scale(1.1)',
+		}
+	},
+
+	handleActive: {
+		backgroundColor: COLORS.BLUE.lighten(1).toString(),
+		transform: 'translate(-50%, -50%) scale(1.05)',
+
+		':hover': {
+			backgroundColor: COLORS.BLUE.lighten(1).toString(),
+			transform: 'translate(-50%, -50%) scale(1.05)',
 		}
 	}
 });
---
+++ src/app/ts/components/ui/RheostatHandle.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { css, StyleDeclaration, StyleSheet } from 'aphrodite/no-important';
+import { ObjectsStore } from '../../stores/ObjectsStore';
+import IObject = ObjectsStore.IObject;
+
+interface IProps {
+
+}
+
+export class RheostatHandle extends React.PureComponent<IProps, {}> {
+	public render() {
+		return (
+			null
+		);
+	}
+}
+
+const styles = StyleSheet.create({});
