c32dc5cae7ccc459c0fd5906b27d5b1ed1bc6cff
Ruslan Shashkov <ruslanchek@icloud.com>
2018-08-31 02:23:19 +0300

    FIx

--- src/app/ts/components/ui/Favorite.tsx
+++ src/app/ts/components/ui/Favorite.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { css, StyleSheet } from 'aphrodite/no-important';
 import { EIcon, EIconType, Icon } from '../common/Icon';
 import { COLORS } from '../../theme';
+import styled, { css, keyframes } from 'react-emotion';
+
 interface IProps {
 	id: number;
 	title: string;
@@ -17,22 +18,22 @@ const SIZE: number = 20;
 
 export class Favorite extends React.PureComponent<IProps, IState> {
 	public state: IState = {
-		animated: false
+		animated: false,
 	};
 
 	private animationTimeout = null;
 
 	public render() {
 		return (
-			<div
-				className={css(styles.favorite, this.state.animated ? styles.animated : styles.notAnimated)}
+			<Container
+				isAnimated={this.state.animated}
 				onClick={this.click.bind(this)}
 			>
 				{this.props.isFavorite ?
 					<Icon icon={EIcon.Favorite} type={EIconType.Default} size={SIZE} color={COLORS.WHITE}/> :
 					<Icon icon={EIcon.Favorite} type={EIconType.TwoTone} size={SIZE} color={COLORS.RED}/>
 				}
-			</div>
+			</Container>
 		);
 	}
 
@@ -45,55 +46,57 @@ export class Favorite extends React.PureComponent<IProps, IState> {
 		event.stopPropagation();
 
 		this.setState({
-			animated: true
+			animated: true,
 		}, () => {
 			this.animationTimeout = setTimeout(() => {
 				this.setState({
-					animated: false
+					animated: false,
 				});
 			}, ANIMATION_TIME);
 		});
 	}
 }
 
-const translateKeyframes = {
-	'0%': {
-		transform: 'scale(1)',
-	},
+interface IRootProps {
+	isAnimated: boolean;
+}
 
-	'33%': {
-		transform: 'scale(0.9)',
-	},
-
-	'66%': {
-		transform: 'scale(1.2)',
-	},
-
-	'100%': {
-		transform: 'scale(1)',
-	},
-};
-
-const styles = StyleSheet.create({
-	favorite: {
-		width: SIZE,
-		height: SIZE,
-		cursor: 'pointer',
-		transition: 'opacity .2s',
-		userSelect: 'none',
-
-		':hover': {
-			opacity: 0.75
-		}
-	},
-
-	animated: {
-		animationName: [translateKeyframes],
-		animationDuration: `${ANIMATION_TIME}ms`,
-		animationIterationCount: 1,
-	},
-
-	notAnimated: {
-
+const translateKeyframes = keyframes`
+	0% {
+		transform: scale(1);
 	}
-});
+
+	33% {
+		transform: scale(.9);
+	}
+
+	66% {
+		transform: scale(1.2);
+	}
+
+	100% {
+		transform: scale(1);
+	}
+`;
+
+const Container = styled('div')<IRootProps>`
+  width: ${SIZE}px;
+	height: ${SIZE}px;
+	cursor: pointer;
+	transition: opacity .2s;
+	user-select: none;
+
+	&:hover {
+		opacity: .75;
+	}
+	
+	${(props: IRootProps) => {
+		if (props.isAnimated) {
+			return css`
+				animation-name: ${translateKeyframes};
+				animation-duration: ${ANIMATION_TIME}ms;
+				animation-iteration-count: 1;
+			`;
+		}
+	}}
+`;
