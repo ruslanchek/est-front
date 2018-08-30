63ad2b6db431dd39fbcb11e69496ae6828539345
Ruslan Shashkov <ruslanchek@icloud.com>
2018-08-31 02:09:57 +0300

    FIx

--- src/app/ts/components/ui/Toast.tsx
+++ src/app/ts/components/ui/Toast.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { css, StyleSheet } from 'aphrodite/no-important';
 import { THEME } from '../../theme';
 import { ToastContainer } from 'react-toastify';
 import Transition from 'react-transition-group/Transition';
+import { css, keyframes } from 'emotion';
 
 interface IProps {
 
@@ -18,10 +18,10 @@ const ZoomInAndOut = ({ children, position, ...props }) => (
 	<Transition
 		{...props}
 		timeout={ANIMATION_DURATION}
-		onEnter={ node => node.classList.add(css(styles.in))}
+		onEnter={ node => node.classList.add(animateIn)}
 		onExit={node => {
-			node.classList.remove(css(styles.in));
-			node.classList.add(css(styles.out));
+			node.classList.remove(animateIn);
+			node.classList.add(animateOut);
 		}}
 	>
 		{children}
@@ -33,7 +33,7 @@ export class Toast extends React.PureComponent<IProps, IState> {
 		return (
 			<ToastContainer
 				transition={ZoomInAndOut}
-				className={css(styles.container)}
+				className={container}
 				closeOnClick={true}
 				closeButton={<span/>}
 			/>
@@ -41,54 +41,51 @@ export class Toast extends React.PureComponent<IProps, IState> {
 	}
 }
 
-const translateKeyframesIn = {
-	from: {
-		opacity: 0,
-	},
-
-	to: {
-		opacity: 1,
+const translateKeyframesIn = keyframes`
+	from {
+		opacity: 0;
 	}
-};
 
-const translateKeyframesOut = {
-	from: {
-		opacity: 1,
-	},
-
-	to: {
-		opacity: 0,
+	to {
+		opacity: 1;
 	}
-};
+`;
 
-const styles = StyleSheet.create({
-	container: {
-		position: 'fixed',
-		bottom: THEME.NAV_HEIGHT + THEME.SECTION_PADDING_V,
-		left: 0,
-		zIndex: 1000,
-		width: '100%',
-		display: 'flex',
-		justifyContent: 'center',
-		boxSizing: 'border-box',
-		flexDirection: 'column',
-		alignItems: 'center'
-	},
-
-	in: {
-		animationName: [translateKeyframesIn],
-		animationDuration: `${ANIMATION_DURATION}ms`,
-		animationIterationCount: 1,
-		animationFillMode: 'bacwards',
-		opacity: 1
-	},
-
-	out: {
-		animationName: [translateKeyframesOut],
-		animationDuration: `${ANIMATION_DURATION}ms`,
-		animationIterationCount: 1,
-		animationFillMode: 'bacwards',
-		opacity: 0
+const translateKeyframesOut = keyframes`
+	from {
+		opacity: 1;
 	}
-});
+	
+	to {
+		opacity: 0;
+	}
+`;
 
+const container = css`
+  position: fixed;
+	bottom: ${THEME.NAV_HEIGHT + THEME.SECTION_PADDING_V}px;
+	left: 0;
+	z-index: 1000;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	box-sizing: border-box;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const animateIn = css`
+  animation-name: ${translateKeyframesIn};
+	animation-duration: ${ANIMATION_DURATION}ms;
+	animation-iteration-count: 1;
+	animation-fill-mode: backwards;
+	opacity: 1;
+`;
+
+const animateOut = css`
+  animation-name: ${translateKeyframesOut};
+	animation-duration: ${ANIMATION_DURATION}ms;
+	animation-iteration-count: 1;
+	animation-fill-mode: backwards;
+	opacity: 0
+`;
